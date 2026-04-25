5afdb86ebacc3bd3377451129bc50f6e2c1318e9
Fabio Masunaga <fabiomasunaga26@gmail.com>
2026-04-25 20:34:37 -0300

    fix: cadastro pessoa completo corrigido

--- src/pages/CadastroPessoa.jsx
+++ src/pages/CadastroPessoa.jsx
@@ -142,7 +142,11 @@ export default function CadastroPessoa() {
 
       {/* NOME */}
       <label style={label}>Nome *</label>
-      <input value={nome} onChange={(e) => setNome(e.target.value)} style={input} />
+      <input
+        value={nome}
+        onChange={(e) => setNome(e.target.value)}
+        style={input}
+      />
 
       {/* DATA */}
       <label style={label}>Data de nascimento</label>
@@ -159,3 +163,135 @@ export default function CadastroPessoa() {
         }}
         style={input}
       />
+
+      {/* TIPO SANGUÍNEO */}
+      <label style={label}>Tipo sanguíneo</label>
+      <select
+        value={tipoSanguineo}
+        onChange={(e) => setTipoSanguineo(e.target.value)}
+        style={input}
+      >
+        <option value="">Selecione</option>
+        <option>O+</option>
+        <option>O-</option>
+        <option>A+</option>
+        <option>A-</option>
+        <option>B+</option>
+        <option>B-</option>
+        <option>AB+</option>
+        <option>AB-</option>
+      </select>
+
+      {/* CONTATO 1 */}
+      <label style={label}>Contato principal *</label>
+      <input
+        placeholder="(99)99999-9999"
+        value={telefone1}
+        onChange={(e) =>
+          setTelefone1(e.target.value.replace(/[^0-9()\- ]/g, ""))
+        }
+        style={input}
+      />
+
+      <input
+        placeholder="Nome contato"
+        value={tutor1Nome}
+        onChange={(e) => setTutor1Nome(e.target.value)}
+        style={input}
+      />
+
+      {/* CONTATO 2 */}
+      <label style={label}>Contato secundário</label>
+      <input
+        placeholder="(99)99999-9999"
+        value={telefone2}
+        onChange={(e) =>
+          setTelefone2(e.target.value.replace(/[^0-9()\- ]/g, ""))
+        }
+        style={input}
+      />
+
+      <input
+        placeholder="Nome contato"
+        value={tutor2Nome}
+        onChange={(e) => setTutor2Nome(e.target.value)}
+        style={input}
+      />
+
+      {/* SAÚDE */}
+      <label style={label}>Comorbidades</label>
+      <input value={comorbidades} onChange={(e) => setComorbidades(e.target.value)} style={input} />
+
+      <label style={label}>Alergias</label>
+      <input value={alergias} onChange={(e) => setAlergias(e.target.value)} style={input} />
+
+      <label style={label}>Medicamentos</label>
+      <input value={medicamentos} onChange={(e) => setMedicamentos(e.target.value)} style={input} />
+
+      {/* BOTÃO */}
+      <button onClick={salvar} disabled={salvando} style={btnSalvar}>
+        {salvando ? "Salvando..." : "Salvar"}
+      </button>
+
+      <p style={obs}>*Obrigatório</p>
+
+    </Container>
+  );
+}
+
+/* 🎨 ESTILOS */
+
+const fotoCircle = {
+  width: 120,
+  height: 120,
+  borderRadius: "50%",
+  background: "#ffeaea",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  margin: "20px auto",
+  cursor: "pointer",
+};
+
+const imgCircle = {
+  width: "100%",
+  height: "100%",
+  borderRadius: "50%",
+  objectFit: "cover",
+};
+
+const fotoTexto = {
+  color: "#ff3b3b",
+  fontWeight: 600,
+};
+
+const input = {
+  width: "100%",
+  padding: 12,
+  borderRadius: 10,
+  border: "1px solid #ddd",
+  marginTop: 5,
+  marginBottom: 10,
+};
+
+const label = {
+  marginTop: 10,
+  display: "block",
+};
+
+const btnSalvar = {
+  width: "100%",
+  padding: 15,
+  borderRadius: 12,
+  background: "#ff3b3b",
+  color: "#fff",
+  border: "none",
+  marginTop: 20,
+};
+
+const obs = {
+  textAlign: "center",
+  fontSize: 12,
+  color: "#999",
+  marginTop: 10,
+};
